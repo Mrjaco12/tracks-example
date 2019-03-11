441bc1b5630f909dcb4622a4b287d302f78b47b2
Jacob <realshin@gmail.com>
2019-03-11 17:33:40 -0400

    First Commit

---
+++ App.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  onButtonPress() {
+    TrackPlayer.updateOptions({
+      stopWithApp: false,
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      ]
+    });
+    
+    TrackPlayer.setupPlayer().then(async () => {    
+      await TrackPlayer.add([{
+          id: 2,
+          url: require('./track39.mp3'),
+          title: 'Title 2',
+          artist: 'Artist 2',
+      },{
+          id: 1,
+          url: require('./track06.mp3'),
+          title: 'Title 1',
+          artist: 'Artist 1',
+      }]);
+      TrackPlayer.play();
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+
+        <Button title="Click It" onPress={this.onButtonPress} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
